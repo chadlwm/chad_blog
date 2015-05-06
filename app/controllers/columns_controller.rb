44804491ae84c1441d66c71f3da8b477d6d7f11a
chad <chad_lwm@hotmail.com>
2015-05-06 12:10:23 +0800

    add page header

--- app/controllers/columns_controller.rb
+++ app/controllers/columns_controller.rb
@@ -1,7 +1,7 @@
 class ColumnsController < ApplicationController
   def show
-  	column = Column.find_by_slug(params[:slug])
-  	@posts = column.posts.includes(:author).order("created_at desc")
+  	@column = Column.find_by_slug(params[:slug])
+  	@posts = @column.posts.includes(:author).order("created_at desc")
   	@tags = Post.tag_counts_on(:tags)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
-  	user = User.find_by_name(params[:name])
-  	@posts = user.posts.order("created_at desc")
+  	@user = User.find_by_name(params[:name])
+  	@posts = @user.posts.order("created_at desc")
   	@tags = Post.tag_counts_on(:tags)
   end
 end
