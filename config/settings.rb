69390075daacccc4d82c71f66917cdcb29566074
chad <chad_lwm@hotmail.com>
2015-05-15 10:38:41 +0800

    修改favicon.ico引用

--- config/settings.rb
+++ config/settings.rb
@@ -1,7 +1,7 @@
 settings.site = {
   title: 'Techer Man',
   description: 'Tech Man 是一个的技术博客， Enjoy Your Time!',
-  avatar: 'http://techer.info/uploads/user/avatar/1/avatar92.jpg',
+  avatar: 'http://techer.info/uploads/user/avatar/1/small_thumb_avatar92.jpg',
   url: 'http://www.techer.info'
 }
 
